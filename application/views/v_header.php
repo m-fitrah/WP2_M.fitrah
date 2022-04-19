4edf3a587e913bdc4afc51e80f49dbdc0f436a38
m fitrah <rajwaizati12@gmail.com>
2022-04-19 22:38:33 +0700

    hallo

---
+++ application/views/v_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Prog II | Merancang Template Sederhana Dengan Codeigneter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/assets/css/stylebuku.css">
+</head>
+<body style="background:white">
+    <div id="wrapper">
+        <header style="background:black">
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan Codeigneter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().  'web/' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url().  'web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+</body>
+</html>
