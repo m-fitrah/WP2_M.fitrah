4d5e346788aeccd775d49aea393a144ad82bf014
m fitrah <rajwaizati12@gmail.com>
2022-04-25 06:58:10 +0700

    Matakuliah 2

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -14,10 +14,10 @@ class Matakuliah extends CI_Controller
             'min_lenght' => 'Kode terlalu pendek'
         ]);
         
-        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]' [
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]' "[
             'required' => 'Nama Matakuliah Harus Diisi',
             'min_lenght' => 'Nama terlalu pendek'
-        ]);
+        ])";
         
         if ($this->form_validation->run()! = true) {
             $this->load->view('view-form-matakuliah');
